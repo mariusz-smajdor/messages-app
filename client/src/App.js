e3893c4d54d8a5b188d78ef624ec1b2bca00accf
Mariusz <smajdormariusz98@gmail.com>
2022-08-05 19:52:12 +0200

    Form into separate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,104 +1,31 @@
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 
 import { useMessages } from './hooks/useMessages';
-import { INITIAL_MSG_STATE } from './initialMsgState';
+import Form from './components/Form';
 
 function App() {
-  const [isSender, setIsSender] = useState(false);
-
   const {
     fetchMessages,
     sendMessage,
     setMsgInfo,
+    setIsSender,
+    isSender,
     statusInfo,
-    messages,
     msgInfo,
+    messages,
   } = useMessages();
-
-  function handleChange(e) {
-    const { name, value } = e.target;
-
-    setMsgInfo(prev => {
-      return {
-        ...prev,
-        [name]: value,
-      };
-    });
-  }
-
-  function checkSender(e) {
-    e.preventDefault();
-    fetchMessages();
-
-    setIsSender(true);
-  }
-
-  function changeSender() {
-    setIsSender(false);
-    setMsgInfo(INITIAL_MSG_STATE);
-  }
 
   return (
     <Fragment>
-      <form className='container-sm mt-5 bg-light rounded shadow px-3 py-5'>
-        {!isSender ? (
-          <Fragment>
-            <h2>Your Username</h2>
-            <input
-              name='sender'
-              className='form-control mt-3'
-              placeholder='Your Username'
-              onChange={handleChange}
-            />
-            <button
-              className='btn btn-primary mt-3 w-100'
-              onClick={checkSender}
-            >
-              Submit
-            </button>
-          </Fragment>
-        ) : (
-          <div className='d-flex justify-content-between'>
-            <h2>{msgInfo.sender}, send a message.</h2>
-            <button className='btn btn-dark' onClick={changeSender}>
-              Change User
-            </button>
-          </div>
-        )}
-        {isSender && (
-          <Fragment>
-            <input
-              name='recipient'
-              className='form-control mt-3'
-              placeholder='Recipient'
-              onChange={handleChange}
-              required
-            />
-            <input
-              name='title'
-              className='form-control mt-3'
-              placeholder='Title'
-              onChange={handleChange}
-              required
-            />
-            <textarea
-              name='message'
-              className='form-control mt-3'
-              placeholder='Message'
-              onChange={handleChange}
-              rows='10'
-              required
-            />
-            <button
-              className='btn btn-primary mt-3 w-100'
-              onClick={sendMessage}
-            >
-              Send Message
-            </button>
-            <p>{statusInfo}</p>
-          </Fragment>
-        )}
-      </form>
+      <Form
+        fetchMessages={fetchMessages}
+        sendMessage={sendMessage}
+        setMsgInfo={setMsgInfo}
+        setIsSender={setIsSender}
+        isSender={isSender}
+        statusInfo={statusInfo}
+        msgInfo={msgInfo}
+      />
       {isSender && (
         <div className='container-sm my-5 bg-light rounded shadow px-3 py-5'>
           <h2>Your received messages</h2>
---
+++ client/src/components/Form.js
@@ -0,0 +1,94 @@
+import { Fragment } from 'react';
+
+import { INITIAL_MSG_STATE } from '../initialMsgState';
+
+function Form({
+  fetchMessages,
+  setIsSender,
+  setMsgInfo,
+  msgInfo,
+  isSender,
+  statusInfo,
+  sendMessage,
+}) {
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setMsgInfo(prev => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
+
+  function checkSender(e) {
+    e.preventDefault();
+
+    fetchMessages();
+    setIsSender(true);
+  }
+
+  function changeSender() {
+    setIsSender(false);
+    setMsgInfo(INITIAL_MSG_STATE);
+  }
+
+  return (
+    <form className='container-sm mt-5 bg-light rounded shadow px-3 py-5'>
+      {!isSender ? (
+        <Fragment>
+          <h2>Your Username</h2>
+          <input
+            name='sender'
+            className='form-control mt-3'
+            placeholder='Your Username'
+            onChange={handleChange}
+          />
+          <button className='btn btn-primary mt-3 w-100' onClick={checkSender}>
+            Submit
+          </button>
+        </Fragment>
+      ) : (
+        <div className='d-flex justify-content-between'>
+          <h2>{msgInfo.sender}, send a message.</h2>
+          <button className='btn btn-dark' onClick={changeSender}>
+            Change User
+          </button>
+        </div>
+      )}
+      {isSender && (
+        <Fragment>
+          <input
+            name='recipient'
+            className='form-control mt-3'
+            placeholder='Recipient'
+            onChange={handleChange}
+            required
+          />
+          <input
+            name='title'
+            className='form-control mt-3'
+            placeholder='Title'
+            onChange={handleChange}
+            required
+          />
+          <textarea
+            name='message'
+            className='form-control mt-3'
+            placeholder='Message'
+            onChange={handleChange}
+            rows='10'
+            required
+          />
+          <button className='btn btn-primary mt-3 w-100' onClick={sendMessage}>
+            Send Message
+          </button>
+          <p>{statusInfo}</p>
+        </Fragment>
+      )}
+    </form>
+  );
+}
+
+export default Form;
